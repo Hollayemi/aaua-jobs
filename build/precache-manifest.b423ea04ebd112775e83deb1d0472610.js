38f6a6cdc0e64cd752c4ab842276ea45cb1df96b
stephanyemmitty <stephanyemmitty@gmail.com>
2024-10-06 09:55:28 +0100

    aaua university project for quick jobs

---
+++ build/precache-manifest.b423ea04ebd112775e83deb1d0472610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d87e1eb09759e3adb6f96dee476c651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dbf005c7cc6d60db69e",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "748844d035b349975c75",
+    "url": "/static/css/main.2871f1ff.chunk.css"
+  },
+  {
+    "revision": "4dbf005c7cc6d60db69e",
+    "url": "/static/js/2.975e5e46.chunk.js"
+  },
+  {
+    "revision": "71715bfa2dbcd86b3f8832da551bc13b",
+    "url": "/static/js/2.975e5e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748844d035b349975c75",
+    "url": "/static/js/main.a9a41fc6.chunk.js"
+  },
+  {
+    "revision": "7daa7f2e6a3851b61941",
+    "url": "/static/js/runtime-main.fa560a86.js"
+  }
+]);
